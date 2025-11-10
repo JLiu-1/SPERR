9cd13f7efb1fc276f4432f9faad7948185851b08
Jliu-1 <jliu217@central.uh.edu>
2025-11-10 01:52:08 -0600

    upd

--- src/SPECK_FLT.cpp
+++ src/SPECK_FLT.cpp
@@ -489,7 +489,7 @@ FIXED_RATE_HIGH_PREC_LABEL:
         return rtn;
     }
   }
-  sperr::write_n_bytes("sperr_quant_codes.test", m_vals_ui * sizeof(int), m_vals_ui.data());
+  sperr::write_n_bytes("sperr_quant_codes.test", m_vals_ui.size() * sizeof(int), m_vals_ui.data());
   // Step 4: Integer SPECK encoding
   m_instantiate_encoder();
   if (m_mode == CompMode::Rate) {

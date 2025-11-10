85da1ab1b93f23cd03356b916b6619f7c887d678
Jliu-1 <jliu217@central.uh.edu>
2025-11-10 13:58:18 -0600

    upd

--- include/CDF97_f.h
+++ include/CDF97_f.h
@@ -30,7 +30,7 @@ class CDF97_F {
   // Output
   //
   auto view_data() const -> const vecf_type&;
-  auto release_data() -> vecd_type&&;
+  auto release_data() -> vecd_type;
   auto get_dims() const -> std::array<size_t, 3>;  // In 2D case, the 3rd value equals 1.
 
   //

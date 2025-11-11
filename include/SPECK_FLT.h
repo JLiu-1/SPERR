c2ca2f62298c83f38c77b2944bf4f8b8ce9e6a1f
Jliu-1 <jliu217@central.uh.edu>
2025-11-11 00:28:31 -0600

    float

--- include/SPECK_FLT.h
+++ include/SPECK_FLT.h
@@ -6,6 +6,7 @@
 //
 
 #include "CDF97.h"
+#include "CDF97_f.h"
 #include "Conditioner.h"
 #include "Outlier_Coder.h"
 #include "SPECK_INT.h"
@@ -79,7 +80,7 @@ class SPECK_FLT {
   Bitmask m_sign_array;
   std::vector<vecd_type> m_hierarchy;  // multi-resolution decoding
 
-  CDF97 m_cdf;
+  CDF97_F m_cdf;
   Conditioner m_conditioner;
   Outlier_Coder m_out_coder;
 

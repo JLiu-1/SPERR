905ee7f366f6a9be61fbe65ba2fc1580fb10d509
Jliu-1 <jliu217@central.uh.edu>
2025-11-10 21:52:55 -0600

    upd

--- include/SPECK_FLT.h
+++ include/SPECK_FLT.h
@@ -80,7 +80,7 @@ class SPECK_FLT {
   vecf_type m_vals_f;//new
   condi_type m_condi_bitstream;
   Bitmask m_sign_array;
-  std::vector<vecd_type> m_hierarchy;  // multi-resolution decoding
+  std::vector<vecf_type> m_hierarchy;  // multi-resolution decoding
 
   CDF97_F m_cdf;
   //CDF97_F m_cdf_f;

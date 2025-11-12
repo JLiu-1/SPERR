7ff76c050f41109df2736825464fb709f16723cc
Jliu-1 <jliu217@central.uh.edu>
2025-11-12 01:14:16 -0600

    new q

--- src/CDF97.cpp
+++ src/CDF97.cpp
@@ -211,7 +211,7 @@ void sperr::CDF97::quantize_3D(const std::vector<double> & vals_d, double q)
     
   }
   auto cur_q = q_hierarchy[0];
-  std::cout<<x<<" "<<y<<" "<<z<<" "<<last_x<<" "<<last_y<<" "<<last_z<<" "<<cur_q<<std::endl;
+  std::cout<<last_x<<" "<<last_y<<" "<<last_z<<" "<<cur_q<<std::endl;
   for (size_t i = 0; i < last_z; i++){
     for (size_t j = 0; j < last_y; j++){
       for (size_t k = 0; k < last_x; k++){
